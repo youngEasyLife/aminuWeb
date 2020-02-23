7aa3a7762e18ac9c6445ed80265613a908483f06
王佩阳 <365597937@qq.com>
2020-02-23 19:48:32 +0800

    星空

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,53 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="stars">
+    <div v-for="(item,index) in starsCount" :key="index" class="star" ref="star">
+    </div>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'App'
+  data () {
+    return {
+      starsCount: 800,
+      distance: 800
+    }
+  },
+  mounted () {
+    let _this = this
+    let starArr = _this.$refs.star
+    console.log(starArr)
+    starArr.forEach(item => {
+      var speed = 0.2 + (Math.random() * 1)
+      var thisDistance = _this.distance + (Math.random() * 300)
+      item.style.transformOrigin = `0 0 ${thisDistance}px`
+      item.style.transform = `translate3d(0,0,-${thisDistance}px) rotateY(${(Math.random() * 360)}deg) rotateX(${(Math.random() * -50)}deg) scale(${speed},${speed})`
+    })
+  }
 }
 </script>
+<style lang="less" scoped>
+  @keyframes rotate {
+    0%{
+      transform: perspective(400px) rotateZ(20deg) rotateX(-40deg) rotateY(0);
+    }
+    100%{
+      transform: perspective(400px) rotateZ(20deg) rotateX(-40deg) rotateY(-360deg);
+    }
+  }
+  .stars{
+    transform: perspective(500px);
+    transform-style: preserve-3d;
+    position: absolute;
+    perspective-origin: 50% 100%;
+    left: 50%;
+    animation: rotate 90s infinite linear;
+    bottom: 0;
+  }
+  .star{
+    width: 2px;
+    height: 2px;
+    background: #f7f7b8;
+    position: absolute;
+  }
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
